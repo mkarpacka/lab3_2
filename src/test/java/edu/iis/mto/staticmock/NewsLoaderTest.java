b3bda63610d3b8ab557776d85b40e38b5d54def9
mkarpacka <209348@edu.p.lodz.pl>
2019-05-22 18:18:55 +0200

    testLoadConfigurationShouldBeCalledOnce

--- src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
+++ src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
@@ -1,21 +1,33 @@
 package edu.iis.mto.staticmock;
 
 import edu.iis.mto.staticmock.reader.NewsReader;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
 
+import java.util.List;
+
+import static org.hamcrest.EasyMock2Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
-import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.mockito.AdditionalMatchers.not;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.when;
+import static org.powermock.api.mockito.PowerMockito.*;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest( {
         ConfigurationLoader.class,
-        NewsReaderFactory.class
+        NewsReaderFactory.class,
 })
 public class NewsLoaderTest {
 
@@ -24,12 +36,15 @@ public class NewsLoaderTest {
     private IncomingNews incomingNews;
     private NewsReader newsReader;
     private NewsLoader newsLoader;
+    private PublishableNews publishableNews;
+    private NewsReaderFactory newsReaderFactory;
 
     @Before
     public void setUp() {
         newsLoader = new NewsLoader();
         config = new Configuration();
         incomingNews = new IncomingNews();
+
 
         incomingNews.add(new IncomingInfo("SubsciptionType A", SubsciptionType.A));
         incomingNews.add(new IncomingInfo("SubsciptionType B", SubsciptionType.B));
@@ -41,13 +56,19 @@ public class NewsLoaderTest {
         when(ConfigurationLoader.getInstance()).thenReturn(mockConfigurationLoader);
         when(mockConfigurationLoader.loadConfiguration()).thenReturn(new Configuration());
 
-
-        newsReader = () -> incomingNews;
+        newsReader = mock(NewsReader.class);
+        when(newsReader.read()).thenReturn(incomingNews);
 
         mockStatic(NewsReaderFactory.class);
-        when(NewsReaderFactory.getReader("reader")).thenReturn(newsReader);
+        newsReaderFactory = mock(NewsReaderFactory.class);
+        when(NewsReaderFactory.getReader(Mockito.any())).thenReturn(newsReader);
+    }
 
+    @Test
+    public void testLoadConfigurationShouldBeCalledOnce(){
 
+        publishableNews = newsLoader.loadNews();
+        verify(mockConfigurationLoader, times(1)).loadConfiguration();
     }
 
 }
